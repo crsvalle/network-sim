51b45484b04d8d90ff9d43013c31ddb214d7c0d9
crsvalle <cristian.valle@pursuit.org>
2025-04-19 23:13:47 -0400

    made graph correlate an office

--- src/constants/defaultTopology.js
+++ src/constants/defaultTopology.js
@@ -1,10 +1,33 @@
+// src/constants/defaultTopology.js
 
 const defaultTopology = {
-    '192.168.1.1': { '192.168.1.2': 1, '192.168.1.3': 4 },
-    '192.168.1.2': { '192.168.1.3': 2, '192.168.1.4': 5 },
-    '192.168.1.3': { '192.168.1.4': 1 },
-    '192.168.1.4': {},
-  };
-  
-  export default defaultTopology;
-  
+  // Core Router
+  '10.0.0.1': {
+    '10.0.1.1': 1, // Switch A
+    '10.0.2.1': 1, // Switch B
+  },
+
+  // Switch A (Office A)
+  '10.0.1.1': {
+    '10.0.1.2': 1, // Host A1
+    '10.0.1.3': 1, // Host A2
+    '10.0.1.4': 1, // Printer A
+  },
+
+  // Switch B (Office B)
+  '10.0.2.1': {
+    '10.0.2.2': 1, // Host B1
+    '10.0.2.3': 1, // Host B2
+    '10.0.2.4': 1, // Printer B
+  },
+
+  // End Devices
+  '10.0.1.2': {}, // Host A1
+  '10.0.1.3': {}, // Host A2
+  '10.0.1.4': {}, // Printer A
+  '10.0.2.2': {}, // Host B1
+  '10.0.2.3': {}, // Host B2
+  '10.0.2.4': {}, // Printer B
+};
+
+export default defaultTopology;
